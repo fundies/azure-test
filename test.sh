ca8aeb9e884e4fad4382455489641c1dbf377957
Greg Williamson <fundies@users.noreply.github.com>
2021-03-22 19:32:05 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-sudo apt-get install openbox
+sudo apt-get -y install libboost-program-options-dev pulseaudio libyaml-cpp-dev libpugixml-dev rapidjson-dev libprotobuf-dev protobuf-compiler zlib1g-dev libglm-dev libalure-dev libvorbisfile3 libvorbis-dev libdumb1-dev libgtk2.0-dev zenity kdialog libpng-dev libbox2d-dev libbullet-dev libfreetype6-dev libsdl2-dev libdrm-dev libgbm-dev
+sudo apt-get -y install openbox
+
+git clone https://github.com/enigma-dev/enigma-dev.git
+cd enigma-dev
+make all
+
 export ASAN_OPTIONS=detect_leaks=0;
 export LIBGL_ALWAYS_SOFTWARE=1
 export DISPLAY=:99.0
 Xvfb :99 -s "-screen 0 1024x768x24" &
 openbox-session &
 sleep 5
+./test-runner
