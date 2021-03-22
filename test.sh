8f244686caa64d04a2094a5001ab288014fc839e
Greg Williamson <fundies@users.noreply.github.com>
2021-03-22 19:25:57 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sudo apt-get install openbox
+export ASAN_OPTIONS=detect_leaks=0;
+export LIBGL_ALWAYS_SOFTWARE=1
 export DISPLAY=:99.0
 Xvfb :99 -s "-screen 0 1024x768x24" &
 openbox-session &
