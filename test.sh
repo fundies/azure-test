9828bb69e3f961d59802292b4299dfdc72a95a14
Greg Williamson <fundies@users.noreply.github.com>
2021-03-22 20:25:16 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt-get -y install libboost-program-options-dev pulseaudio libyaml-cpp-dev libpugixml-dev rapidjson-dev libprotobuf-dev protobuf-compiler zlib1g-dev libglm-dev libalure-dev libvorbisfile3 libvorbis-dev libdumb1-dev libgtk2.0-dev zenity kdialog libpng-dev libbox2d-dev libbullet-dev libfreetype6-dev libsdl2-dev libdrm-dev libgbm-dev
+sudo apt-get update
+sudo apt-get -y install libboost-program-options-dev pulseaudio libyaml-cpp-dev libpugixml-dev rapidjson-dev libprotobuf-dev protobuf-compiler zlib1g-dev libglm-dev libalure-dev libvorbisfile3 libvorbis-dev libdumb1-dev libgtk2.0-dev zenity kdialog libpng-dev libbox2d-dev libbullet-dev libfreetype6-dev libsdl2-dev libdrm-dev libgbm-dev googletest
 sudo apt-get -y install openbox
 
 git clone https://github.com/enigma-dev/enigma-dev.git
