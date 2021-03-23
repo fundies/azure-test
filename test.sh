ffadbc7955583850a0d5fe42f4655c9cf3f2501e
Greg Williamson <fundies@users.noreply.github.com>
2021-03-22 21:16:56 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,19 +2,20 @@
 
 sudo apt-get update
 sudo apt-get -y install libboost-program-options-dev pulseaudio libyaml-cpp-dev libpugixml-dev rapidjson-dev libprotobuf-dev protobuf-compiler zlib1g-dev libglm-dev libalure-dev libvorbisfile3 libvorbis-dev libdumb1-dev libgtk2.0-dev zenity kdialog libpng-dev libbox2d-dev libbullet-dev libfreetype6-dev libsdl2-dev libdrm-dev libgbm-dev libgtest-dev libepoxy-dev libegl1-mesa-dev libgles2-mesa-dev libglew-dev libxrandr-dev libxinerama-dev
-sudo apt-get -y install openbox
+sudo apt-get -y install openbox mesa-utils
 
-git clone https://github.com/enigma-dev/enigma-dev.git
-cd enigma-dev
-git checkout cifixes
-make all
+#git clone https://github.com/enigma-dev/enigma-dev.git
+#cd enigma-dev
+#git checkout cifixes
+#make all
 
 export ASAN_OPTIONS=detect_leaks=0;
 export LIBGL_ALWAYS_SOFTWARE=1
 export DISPLAY=:99.0
-Xvfb :99 -s "-screen 0 1024x768x24" &
+#Xvfb :99 -s "-screen 0 1024x768x24" &
 openbox-session &
 sleep 5
-./emake -o /tmp/test -r &
+#./emake -o /tmp/test -r &
+glxgears
 sleep 5
 wmctrl -l
