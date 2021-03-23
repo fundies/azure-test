6a6d43760a3328c187630a8266e1579f84fa3184
Greg Williamson <fundies@users.noreply.github.com>
2021-03-22 21:45:58 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 sudo apt-get -y install libboost-program-options-dev pulseaudio libyaml-cpp-dev libpugixml-dev rapidjson-dev libprotobuf-dev protobuf-compiler zlib1g-dev libglm-dev libalure-dev libvorbisfile3 libvorbis-dev libdumb1-dev libgtk2.0-dev zenity kdialog libpng-dev libbox2d-dev libbullet-dev libfreetype6-dev libsdl2-dev libdrm-dev libgbm-dev libgtest-dev libepoxy-dev libegl1-mesa-dev libgles2-mesa-dev libglew-dev libxrandr-dev libxinerama-dev
-sudo apt-get -y install openbox mesa-utils wmctrl xdotool
+sudo apt-get -y install openbox mesa-utils wmctrl xdotool xll-apps
 
 #git clone https://github.com/enigma-dev/enigma-dev.git
 #cd enigma-dev
@@ -16,6 +16,6 @@ Xvfb :99 -s "-screen 0 1024x768x24" &
 openbox-session &
 sleep 5
 #./emake -o /tmp/test -r &
-glxgears &
+xclock &
 sleep 5
 wmctrl -l
